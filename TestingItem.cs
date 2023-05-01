16951252d9c3a5ffd840ecbe61caaab989db675c
Sai00107 <131841539+Sai00107@users.noreply.github.com>
2023-05-01 16:42:13 +1000

    Add files via upload

---
+++ TestingItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Swin_Adventure;
+
+namespace TestingSwinAdventure
+{
+    public class TestingItem
+    {
+
+        private Item item;
+
+        [SetUp]
+        public void SettingUp()
+        {
+            item = new Item(new string[] {"sword"}, "a bronze sword","The Mighty Sword yielded by a hero");
+        }
+
+
+        [Test]
+        public void TestItemIsIdentifiable()
+        {
+            Assert.IsTrue(item.AreYou("sword"));
+            Assert.IsFalse(item.AreYou("AnythingElse"));
+        }
+
+        [Test]
+        public void TestShortDescription()
+        {
+            Assert.AreEqual("a bronze sword(sword)", item.ShortDescription);
+        }
+
+        [Test]
+        public void TestFullDescription()
+        {
+            Assert.AreEqual("The Mighty Sword yielded by a hero", item.FullDescription);
+        }
+    }
+}
---
+++ TestingLocations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Swin_Adventure;
+
+namespace TestingSwinAdventure
+{
+    public class TestingLocations
+    {
+        private Locations _location;
+        private Item item;
+
+        [SetUp]
+        public void Setup()
+        {
+            _location = new Locations("space", "Black Hole");
+            item = new Item(new string[] { "hat" }, "a hat", "a cool object.");
+            _location.Inventory.Put(item);
+        }
+
+        [Test]
+        public void TestLocationsisIdentifiable()
+        {
+            Assert.IsTrue(_location.AreYou("room"));
+        }
+
+        [Test]
+        public void TestLocationsLocatesItems()
+        {
+            Assert.AreEqual(item, _location.Locate("hat"));
+        }
+
+        [Test]
+        public void TestLocationsLocatesItself()
+        {
+            Assert.AreEqual(_location, _location.Locate("room"));
+        }
+
+        [Test]
+        public void TestLocationsFullDescription()
+        {
+            Assert.AreEqual("You are in space, Black Hole. You can see: a hat(hat)\n\t", _location.FullDescription);
+        }
+
+        [Test]
+        public void TestLocationsLocatesNothing()
+        {
+            Assert.AreEqual(null, _location.Locate("stick"));
+        }
+    }
+}
